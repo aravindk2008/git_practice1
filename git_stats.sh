08b24976327a81576f0b076b93b7b2fe5973b03b
yukanth <yukanthdn@gmail.com>
2026-06-17 10:44:06 +0000

    removed unwanted space

--- git_stats.sh
+++ git_stats.sh
@@ -5,4 +5,4 @@ git log --format="%an" | sort | uniq -c | sort -rn
 echo "the 3 most recently modified files"
 git log --name-only --format="" | grep -v '^$' | awk '!seen[$0]++' | head -3
 echo "the file that has been changed in the most commits"
-git log --diff -filter=M --name-only --format="" | sort | uniq -c | sort -rn | head -3
+git log --diff-filter=M --name-only --format="" | sort | uniq -c | sort -rn | head -3
